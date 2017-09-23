32fdd87cc4768549dd71e240f218609aa25cd741
jonis.alves <jonis.alves@gmail.com>
2017-09-23 11:09:08 -0300

    inserir e alterar

--- class/Usuario.php
+++ class/Usuario.php
@@ -50,12 +50,7 @@ public function loadById($id){
 	$sql = new Sql();
 	$results = $sql->select("SELECT * FROM tb_usuario WHERE idUsuario = :ID", array(":ID"=>$id));
 	if(count($results)> 0){
-		$row = $results[0];
-
-		$this->setIdusuario($row['idUsuario']);
-		$this->setDeslogin($row['deslogin']);
-		$this->setDessenha($row['dessenha']);
-		$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		$this->setData($results[0]);
 	}
 }
 
@@ -77,16 +72,52 @@ public function login($login, $password){
 	$sql = new Sql();
 	$results = $sql->select("SELECT * FROM tb_usuario WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(":LOGIN"=>$login,":PASSWORD"=>$password));
 	if(count($results)> 0){
-		$row = $results[0];
-
-		$this->setIdusuario($row['idUsuario']);
-		$this->setDeslogin($row['deslogin']);
-		$this->setDessenha($row['dessenha']);
-		$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		$this->setData($results[0]);
+		
 	}else{
 		throw new Exception("Login e/ou senha invalda.");
 		
 	}
+
+}
+
+public function setData($data){
+
+	$this->setIdusuario($data['idUsuario']);
+	$this->setDeslogin($data['deslogin']);
+	$this->setDessenha($data['dessenha']);
+	$this->setDtcadastro(new DateTime($data['dtcadastro']));
+
+}
+
+public function insert(){
+
+	$sql = new Sql();
+	$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha()
+	));
+
+	if(count($results)> 0){
+		$this->setData($results[0]);
+	}
+
+}
+
+public function update($login, $password){
+
+	$this->setDeslogin($login);
+	$this->setDessenha($password);
+
+	$sql = new Sql();
+
+	$sql->query("UPDATE tb_usuario SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+		':LOGIN'=>$this->getDeslogin(),
+		':PASSWORD'=>$this->getDessenha(),
+		':ID'=>$this->getIdusuario()
+
+		));
+
 
 }
 
@@ -95,6 +126,10 @@ public function delete(){
 	$sql = new Sql();
 	$sql->query("DELETE FROM tb_usuario WHERE idusuario = :ID",array(
 		':ID'=>$this->getIdusuario()));
+	$this->setIdusuario(0);
+	$this->setDeslogin("");
+	$this->setDessenha("");
+	$this->setDtcadastro(new DateTime());
 }
 
 public function __toString(){
@@ -104,7 +139,8 @@ public function __toString(){
 		"idusuario"=>$this->getIdusuario(),
 		"deslogin"=>$this->getDeslogin(),
 		"dessenha"=>$this->getDessenha(),
-		"dtcadastro"=>$this->getDtcadastro()->format("d/m/y H:i:s")));
+		"dtcadastro"=>$this->getDtcadastro()->format('d/m/y H:m:s')
+	));
 }
 }
 
--- index.php
+++ index.php
@@ -21,8 +21,28 @@ require_once("config.php");
 //echo json_encode($search);
 
 //carrega usuario usando o login e senha
-$usuario = new Usuario();
-$usuario->login("jonis.alves","123");
-echo $usuario;
+//$usuario = new Usuario();
+//$usuario->login("jonis.alves","123");
+//echo $usuario;
 
+
+//insere um usuario com o metodo inert
+/*
+$aluno = new Usuario();
+
+$aluno->setDeslogin("aluno");
+$aluno->setDessenha("351");
+
+$aluno->insert();
+
+echo $aluno;
+*/
+
+$usuario = new Usuario();
+
+$usuario->loadbyId(6);
+
+$usuario->update("aluno 1", "122345");
+
+echo $usuario;
 ?>
